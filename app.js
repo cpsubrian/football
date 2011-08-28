2d1c6da9850754a0d2fca4a38fa44be89f69de48
Brian Link <cpsubrian@gmail.com>
2011-08-27 18:40:15 -0700

    Redis Sessions and Users!!!!!

--- app.js
+++ app.js
@@ -4,29 +4,28 @@
 var express = require('express'),
     everyauth = require('everyauth'),
     app = module.exports = express.createServer(),
+    RedisStore = require('connect-redis')(express),
+    redis = require('redis'),
+    redisClient = redis.createClient(),
     conf = require('./conf.js'),
     helpers = require('./helpers.js')(app, conf);
-    
-// Setup everyauth.
-var usersById = {};
-var usersByYahooId = {};
-var nextUserId = 0;
 
+// Get a user stored in redis.
 everyauth.everymodule.findUserById( function (userId, callback) {
-  callback(null, usersById[userId]);
+  redisClient.get('users.' + userId, function(err, data) {
+    callback(null, JSON.parse(data));
+  });
 });
 
-function addUser (source, sourceUser) {
-  var user;
-  if (arguments.length === 1) { // password-based
-    user = sourceUser = source;
-    user.id = ++nextUserId;
-    return usersById[nextUserId] = user;
-  } else { // non-password-based
-    user = usersById[++nextUserId] = {id: nextUserId};
+// Add a new user to redis.
+function addUser (source, sourceUser, callback) {  
+  redisClient.incr('next.user.id', function(err, nextId) {
+    var user = {'id' : nextId};
     user[source] = sourceUser;
-  }
-  return user;
+    redisClient.set('users.' + nextId, JSON.stringify(user), function(err, data) {
+      callback(err, user);
+    });
+  });
 }
 
 // Configure yahoo oauth.
@@ -35,7 +34,20 @@ everyauth.yahoo
   .consumerSecret(conf.yahoo.secret)
   .myHostname('http://football.brianthomaslink.com')
   .findOrCreateUser( function (sess, accessToken, accessSecret, yahooUser) {
-    return usersByYahooId[yahooUser.guid] || (usersByYahooId[yahooUser.guid] = addUser('yahoo', yahooUser));
+    var promise = this.Promise();
+    redisClient.get('users.by.yahoo.guid.' + yahooUser.guid, function(err, userId) {
+      if (userId) {
+        redisClient.get('users.' + userId, function(err, data) {
+          promise.fulfill(JSON.parse(data));
+        });
+      }
+      else {
+        addUser('yahoo', yahooUser, function(err, user) {
+          promise.fulfill(user);
+        });
+      }
+    });
+    return promise;
   })
   .redirectPath('/');
 
@@ -49,7 +61,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret:  conf.session.secret}));
+  app.use(express.session({ store: new RedisStore, secret:  conf.session.secret}));
   app.use(everyauth.middleware());
 });
 app.configure('development', function(){
@@ -59,7 +71,7 @@ app.configure('test', function() {
   app.use(express.errorHandler());
 });
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 // Helpers
@@ -70,17 +82,33 @@ everyauth.helpExpress(app);
 app.get('/', function(req, res){
   res.render('index'); 
 });
-app.post('/', function(req, res) {
+app.get('/sandbox', function(req, res) {
+  if (req.user) {
+    res.render('sandbox', {results: 'Results will be here'});
+  }
+  else {
+    res.redirect('/'); 
+  }
+});
+app.post('/sandbox', function(req, res) {
   if (req.user && req.body.api_url) {
+    var api = everyauth.yahoo.oauth;
     var yahoo = req.session.auth.yahoo;
-    everyauth.yahoo.oauth.get(req.body.api_url, yahoo.accessToken, yahoo.accessTokenSecret, function(error, data) {
-      res.render('index', {
-        results: data  
+    var url = req.body.api_url + (req.body.json ? '?format=json' : '');
+    var format = (req.body.json ? 'js' : 'xml');
+    api.get(url, yahoo.accessToken, yahoo.accessTokenSecret, function(error, data) {
+      if (format == 'js') {
+        data = JSON.stringify(JSON.parse(data), null, 2);
+      }
+      res.render('sandbox', {
+        'api_url': req.body.api_url,
+        'results': data,
+        'format': format
       });
     });
   }
   else {
-    res.render('index'); 
+    res.redirect('/'); 
   }
 });
 
--- helpers.js
+++ helpers.js
@@ -6,7 +6,14 @@ module.exports = function(app, conf) {
     // Application title.
     title: conf.title,  
     
-    // Return scripts from settings.js
+    // Return styles from conf.js.
+    headStyles: function() {
+      return conf.styles.map(function(src) {
+        return '<link rel="stylesheet" href="' + src + '" />';
+      }).join("\n");
+    },
+    
+    // Return scripts from conf.js
     headScripts: function() {
       return conf.scripts['head'].map(function(src) {
         return '<script type="text/javascript" src="' + src + '"></script>'
