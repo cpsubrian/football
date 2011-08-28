a1de3ee0dc6dd49400de7f124727dd41a569223b
Brian Link <cpsubrian@gmail.com>
2011-08-27 18:43:09 -0700

    Updated the sample conf.

--- conf-sample.js
+++ conf-sample.js
@@ -7,17 +7,41 @@ module.exports = {
   session: {
     secret: 'your secret here'
   },
+  styles: [
+    'http://twitter.github.com/bootstrap/assets/css/bootstrap-1.1.1.min.css',
+    'http://alexgorbatchev.com/pub/sh/current/styles/shThemeDefault.css',
+    
+    '/css/style.css'
+  ],
   scripts: {
     'head': [
       'http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js',
       //'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js', 
-      //'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.1.7/underscore-min.js',
-      //'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.5.1/backbone-min.js',
+      'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.1.7/underscore-min.js',
+      'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.5.1/backbone-min.js',
       //'http://cdnjs.cloudflare.com/ajax/libs/socket.io/0.7.0/socket.io.min.js', 
-      //'http://html5shim.googlecode.com/svn/trunk/html5.js',     
+      //'http://html5shim.googlecode.com/svn/trunk/html5.js',
+      
+      'http://alexgorbatchev.com/pub/sh/current/scripts/shCore.js',
+      'http://alexgorbatchev.com/pub/sh/current/scripts/shBrushXml.js',
+      'http://alexgorbatchev.com/pub/sh/current/scripts/shBrushJScript.js'
     ],
     'footer': [
       '/js/script.js'       
     ]
+  },
+  yahoo: {
+    key: 'you yahoo consumer key',
+    secret: 'your yahoo consumer secret',
+    api: {
+      2010: {
+        game_key: '242',
+        league_key: 'your league key',
+      },
+      2011: {
+        game_key: '257',
+        league_key: 'your league key',
+      }
+    }
   }
 };
